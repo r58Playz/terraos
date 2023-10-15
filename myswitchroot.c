7c714eb1b9ad23db436fe09985dd791c98b1e91a
Toshit Chawda <r58playz@gmail.com>
2023-10-14 23:54:43 -0700

    better C

--- myswitchroot.c
+++ myswitchroot.c
@@ -1,24 +1,47 @@
 // c :heart_mauve:
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/mount.h>
-#include <sys/stat.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
-int main() {
-  // who needs error checking huh
-  printf("right uhh um i'm gonna have to hack the shim. technical. uhmm you'll need to turn around while i do this.\nturn around, i'll only be a second. if you would mind.\nwould you mind uhh putting your back towards me so i can see only your back\ngo on just turn right the way around so you're not looking at me\n");
-  mkdir("/newroot/mnt", 0770);
-  chdir("/newroot");
-  syscall(SYS_pivot_root,".",".");
-  if(umount2(".", MNT_DETACH)) {
-    printf("failed to umount old root!!\n");
+void xchdir(const char *path) {
+  if(chdir(path)) {
+    printf("failed to chdir to %s: %s\n", path, strerror(errno));
+    exit(1);
   }
-  //mountsystemd();
-  char *argv[] = { "/sbin/init", NULL };
+}
+
+void pivot_root(const char *from, const char *to) {
+  if(syscall(SYS_pivot_root,from,to)) {
+    printf("failed to pivot_root from %s to %s: %s\n", from, to, strerror(errno));
+    exit(1);
+  }
+}
+
+void xumount2(const char *target, int flags) {
+  if(umount2(target, flags)) {
+    printf("failed to umount %s: %s", target, strerror(errno));
+    exit(1);
+  }
+}
+
+void xexecv(const char *path, char *const argv[]) {
+  if(execv(argv[0], argv)) {
+    printf("failed to exec %s: %s", path, strerror(errno));
+  }
+}
+
+int main() {
+  printf("right uhh um i'm gonna have to hack the shim. technical. uhmm you'll need to turn around while i do this.\nturn around, i'll only be a second. if you would mind.\nwould you mind uhh putting your back towards me so i can see only your back\ngo on just turn right the way around so you're not looking at me\n");
+  xchdir("/newroot");
+  pivot_root(".",".");
+  xumount2(".", MNT_DETACH);
   printf("done! hacked.\n");
-  execv(argv[0], argv);
+  char *argv[] = { "/sbin/init", NULL };
+  xexecv(argv[0], argv);
   return 0;
 }
