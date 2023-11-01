28ad83cf268572f059b4a85314bd1cd3bf237a40
Toshit Chawda <r58playz@gmail.com>
2023-10-31 17:54:59 -0700

    more skidproofing

--- create_rootfs.sh
+++ create_rootfs.sh
@@ -32,14 +32,46 @@ if [ $# -le 5 ]; then
 fi
 
 if ! which pacstrap >/dev/null 2>/dev/null; then
-  die "this program requires pacstrap"
+  die "this program requires pacstrap from arch-install-scripts"
 fi
 
 if ! which arch-chroot >/dev/null 2>/dev/null; then
-  die "this program requires arch-chroot"
+  die "this program requires arch-chroot from arch-install-scripts"
 fi
 
+if ! which lsof >/dev/null 2>/dev/null; then
+  die "this program requires lsof"
+fi
+
+if ! which xargs >/dev/null 2>/dev/null; then
+  die "this program requires xargs"
+fi
+
+if test ! -f "${2}"; then
+  die "${2}: No such file"
+fi
+
+if test ! -f "${3}"; then
+  die "${3}: No such file"
+fi
+
+if test ! -f "${4}"; then
+  die "${4}: No such file"
+fi
+
+if test ! -f "${5}"; then
+  die "${5}: No such file"
+fi
+
+if test ! -f "${6}"; then
+  die "${6}: No such file"
+fi
+
+# fix for people not removing the old rootfs before re-creating it
+rm -rf "${1}"
 mkdir "${1}"
+
+if test -d mnt; then rm -r mnt; fi
 
 pacstrap -K "${1}" base linux-firmware networkmanager network-manager-applet xfce4 xfce4-goodies lightdm-gtk-greeter pulseaudio pavucontrol alsa-utils sof-firmware mesa-amber firefox noto-fonts which sudo vim neofetch || die "failed to bootstrap rootfs"
 cp "${4}" "${5}" "${6}" "${1}/" || die "failed to copy packages to root"
@@ -64,6 +96,10 @@ WantedBy=basic.target
 EOT
 arch-chroot "${1}" systemctl enable NetworkManager lightdm kill-frecon || die "failed to enable services"
 
+lsof -t +D "${1}" 2>/dev/null | xargs kill -9 
+
+umount -R "${1}" || die "failed to unmount root bindmount"
+
 SHIM_DEV=$(losetup -Pf --show "${2}")
 
 mkdir mnt || die "failed to create temporary mountpoint"
@@ -71,7 +107,7 @@ mkdir mnt || die "failed to create temporary mountpoint"
 mount "${SHIM_DEV}p3" -o ro mnt || die "failed to mount shim"
 cp -a mnt/lib/firmware "${1}/lib/" || die "failed to copy firmware"
 cp -a mnt/lib/modules  "${1}/lib/" || die "failed to copy modules"
-umount mnt || die "failed to unmount shim"
+umount -f mnt || die "failed to unmount shim"
 
 losetup -d ${SHIM_DEV} || die "failed to remove shim loop device"
 
@@ -79,11 +115,10 @@ RECO_DEV=$(losetup -Pf --show "${3}")
 
 mount "${RECO_DEV}p3" -o ro mnt || die "failed to mount recovery image"
 cp -a mnt/etc/modprobe.d/alsa* "${1}/etc/modprobe.d/" || die "failed to copy alsa drivers"
-umount mnt || die "failed to unmount recovery image"
+umount -f mnt || die "failed to unmount recovery image"
 
 losetup -d ${RECO_DEV} || die "failed to remove recovery image loop device"
 
-rm -r mnt || die "failed to create temporary mountpoint"
+rm -r mnt || die "failed to remove temporary mountpoint"
 
-umount "${1}" || die "failed to unmount root bindmount"
 
